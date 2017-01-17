6fc1fd555befe960176d746f6b2970f65ea9b99e
federico <federico.crivellaro@sine.com>
2017-01-17 11:46:11 +0100

    node modules and shopify parameters

--- utils/credentials.js
+++ utils/credentials.js
@@ -1,14 +1,14 @@
 module.exports = {
 	oauth: {
-		shopify_api_key: '7251f2b8b19d4d9c144cf5c2bde03d40', // Your API key 
-		shopify_shared_secret: '5f93df588305ffb23683a960781a625a', // Your Shared Secret 
+		shopify_api_key: '9f3dc823496745120efebe466e15a44c', // Your API key 
+		shopify_shared_secret: '312594f20fe6b4d3ac0a4a817442c37e', // Your Shared Secret 
 		shopify_scope: 'read_orders',
     	redirect_uri:'install/finish_auth',
     	remove_uri:'install/delete',
 		nonce: '' // you must provide a randomly selected value unique for each authorization request 
 	},
 	path:{
-		prod:'http://shopifyprivateskeleton-01shopify.rhcloud.com/',
+		prod:'http://glsorders-01shopify.rhcloud.com/',
 		dev:'http://localhost:3000/'
 	}
 
