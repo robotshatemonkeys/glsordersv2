6209ed07121ee8a7a5b91cc733b63f7017220acd
Federico Crivellaro <crivellarofederico@gmail.com>
2017-03-01 10:47:33 +0100

    a

--- utils/credentials.js
+++ utils/credentials.js
@@ -5,7 +5,7 @@ module.exports = {
 	oauth: {
 		shopify_api_key: '9f3dc823496745120efebe466e15a44c', // Your API key 
 		shopify_shared_secret: '312594f20fe6b4d3ac0a4a817442c37e', // Your Shared Secret 
-		shopify_scope: 'read_orders',
+		shopify_scope: 'read_orders,write_orders',
     redirect_uri:'install/finish_auth',
 		nonce: '' // you must provide a randomly selected value unique for each authorization request 
 	},
