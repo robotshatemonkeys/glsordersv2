02a3071c70dc1d6083d895a6549bb5cfe3d23e6f
Federico Crivellaro <crivellarofederico@gmail.com>
2017-04-19 10:07:13 +0200

    test

--- routes/print.js
+++ routes/print.js
@@ -79,7 +79,8 @@ router.get('/', function(req, res, next) {
 
 function generateDat(order){
 	console.log(order);
-	let tntDest=buildTnt(order.shipping_address.address2&", "&order.note,tntDestLength);
+	let noteAddress=order.shipping_address.address2+", "+order.note;
+	let tntDest=buildTnt(noteAddress+", "+order.note,tntDestLength);
 	let tntRag=buildTnt(order.shipping_address.name,tntRagLength);
 	let tntInd=buildTnt(order.shipping_address.address1,tntIndLength);
 	let tntCap=buildTnt(order.shipping_address.zip,tntCapLength);
